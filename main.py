80d86deec9566a8ef1cdbda6c4f6ff4e4199a4e0
Maxim Shabanov <nightstalker1973@gmail.com>
2021-11-18 06:15:42 +0300

    Created the basis of the program

---
+++ main.py
@@ -0,0 +1,63 @@
+class FileManager:
+    def __init__(self):
+        pass
+
+    def far(self):
+        pass
+
+    def refresh_dir(self):
+        pass
+
+    def list_dir(self):
+        pass
+
+    def choice_id(self):
+        pass
+
+    def create_dir(self):
+        pass
+
+    def delete_fir(self):
+        pass
+
+    def move_between_dir(self):
+        pass
+
+    def move_up_dir(self):
+        pass
+
+    def create_file(self):
+        pass
+
+    def write_file(self):
+        pass
+
+    def read_file(self):
+        pass
+
+    def delete_file(self):
+        pass
+
+    def copy_file(self):
+        pass
+
+    def move_file(self):
+        pass
+
+    def rename_file(self):
+        pass
+
+    def zip(self):
+        pass
+
+    def unzip(self):
+        pass
+
+
+def main():
+    manager = FileManager()
+    manager.far()
+
+
+if __name__ == '__main__':
+    main()
